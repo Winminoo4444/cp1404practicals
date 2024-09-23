f4f7cd42c06a1fd70da407c11b50d0be381ac5b0
Winminoo <winminoo@my.jcu.edu.au>
2024-09-23 16:24:17 +0800

    Added random code generator

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,21 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
+def main():
+    score = float(input("Enter score: "))
+    print(get_score(score))
+    random_score = random.randint(0, 100)
+    print(f"Random score: {random_score:.2f}")
+    print(get_score(random_score))
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def get_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+main()
