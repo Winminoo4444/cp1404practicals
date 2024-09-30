ae3b9f21fbcde7a78e17796db6ac70f1c14e0bc5
Winminoo <winminoo@my.jcu.edu.au>
2024-09-30 15:24:26 +0800

    Answered questions

---
+++ prac_03/randoms.py
@@ -0,0 +1,15 @@
+"""
+What did you see on line 1?
+smallest number = 5
+largest number = 20
+
+What did you see on line 2?
+smallest number = 5
+largest number = 9
+Could line 2 have produced a 4?
+Line 2 could not have produced 4
+
+What did you see on line 3?
+smallest number = 2.5
+largest number = 5.5
+"""
