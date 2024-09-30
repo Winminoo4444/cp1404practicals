74054d6d47842274d0ba525a133a52de91e8d683
Winminoo <winminoo@my.jcu.edu.au>
2024-09-30 16:35:23 +0800

    Fixed the code

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,10 +2,11 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+when the user enters a value that cannot be converted into integer
 2. When will a ZeroDivisionError occur?
+when the user enters 0 as input in denominator
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
-
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator (can't be zero): "))
@@ -18,7 +19,3 @@ try:
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
 print("Finished.")
-"""
-1. when the user enters a value that cannot be converted into integer
-2. when the user enters 0 as input in denominator
-"""
