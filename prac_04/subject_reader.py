a13e853ad72a20b70777a59dfaaea68f2294e86c
Winminoo <winminoo@my.jcu.edu.au>
2024-10-07 20:11:47 +0800

    Added final function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -4,17 +4,22 @@ Data file -> lists program
 """
 
 FILENAME = "subject_data.txt"
-
-
 def main():
-    data = load_data()
-    print(data)
+    records = get_data()
+    data = print_data(records)
+    final_report(data)
 
-
-def load_data():
+def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
-    for line in input_file:
+    data = input_file.readlines()
+    input_file.close()
+    return data
+
+def print_data(records):
+    """Function for printing all data"""
+    dataset = []
+    for line in records:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
@@ -22,8 +27,12 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+        dataset.append(parts)
+    return dataset
 
-
+def final_report(data):
+    """Function for displaying final report data"""
+    for line in data:
+        print(f"{line[0]} is taught by {line[1]} and has {line[2]} students")
+        print("")
 main()
