1e340d7ed840944eabbebbef67bdd50a72e662cf
Winminoo <winminoo@my.jcu.edu.au>
2024-10-14 21:35:14 +0800

    Complete emails exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,7 +6,7 @@ def main():
         name = get_name(email)
         confirm = input(f"Is your name {name}? (Y/n) ")
         if confirm.upper() != "Y" and confirm != "":
-            name = input("Name: ")
+            name = input("Name:")
         email_to_name[email] = name
         email = input("Email: ")
     for email, name in email_to_name.items():
