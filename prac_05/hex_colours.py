c40ce682ae4d9a02b26ad3ec2684faf4a5b9262a
Winminoo <winminoo@my.jcu.edu.au>
2024-10-14 21:07:44 +0800

    Added error handling for invalid color names

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,12 +1,29 @@
 COLOURS_CODES = {
     "Absolute Zero": "#0048ba",
-    "Amethyst": "#9966cc",
-    "Azure1": "##f0ffff",
+    "Aqua": "#00ffff",
+    "Azure1": "#f0ffff",
     "BlueViolet":"#8a2be2",
     "Baby Blue":"#89cff0",
     "Baker-Miller Pink":"#ff91af",
     "Black Coffee":"#3b2f2f",
     "Brandeis Blue": "#0070ff",
     "Brass": "#b5a642",
-    "BrickRed":"#cb4154"}
+    "Bright Green":"#66ff00"}
 
+def main():
+    """Validating colour names"""
+    color_names = validate_color_names()
+    while color_names != "":
+        color_code = COLOURS_CODES[color_names]
+        print(f"{color_code}")
+        color_names = validate_color_names()
+
+def validate_color_names():
+    """Error checking colour names"""
+    color_names = input("Enter the color name: ").title()
+    while color_names not in COLOURS_CODES:
+        print("Invalid Color Name")
+        color_names = input("Color Name: ").title()
+    return color_names
+
+main()
