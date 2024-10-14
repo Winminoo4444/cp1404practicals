cebf59155e87fc348bec4057ae9f1624fbc6b102
Winminoo <winminoo@my.jcu.edu.au>
2024-10-14 21:03:43 +0800

    Add color name

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+COLOURS_CODES = {
+    "Absolute Zero": "#0048ba",
+    "Amethyst": "#9966cc",
+    "Azure1": "##f0ffff",
+    "BlueViolet":"#8a2be2",
+    "Baby Blue":"#89cff0",
+    "Baker-Miller Pink":"#ff91af",
+    "Black Coffee":"#3b2f2f",
+    "Brandeis Blue": "#0070ff",
+    "Brass": "#b5a642",
+    "BrickRed":"#cb4154"}
+
