91ae2b624c734884ac515ee136150254245b03e6
Winminoo4444 <winminoo@my.jcu.edu.au>
2024-10-28 18:33:14 +0800

    Create Guitar class with attributes for name, year, and cost.

---
+++ prac_06/guitar.py
@@ -0,0 +1,8 @@
+CURRENT_TIME = 2024
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        """Function for the classes"""
+        self.name = name
+        self.year = year
+        self.cost = cost
