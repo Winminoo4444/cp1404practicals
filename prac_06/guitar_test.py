bee37c285b506896907315f8fc093c4833058dec
Winminoo4444 <winminoo@my.jcu.edu.au>
2024-10-28 18:47:42 +0800

    Completed guitar_test.py

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,9 @@
+from prac_06.guitar import Guitar
+
+guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+another_guitar = Guitar("Another Guitar", 2013, 1000)
+print(f"{guitar.name} get_age()-Expected 100. Got {guitar.get_age()}")
+print(f"{another_guitar.name} get_age() - Expected 9. Got {another_guitar.get_age()}")
+print(f"{guitar.name} is_vintage() - Expected True. Got {guitar.is_vintage()}")
+print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
+
