1ad571bef08bc6a6e6babe973a25fc0f6afaf93a
Winminoo4444 <winminoo@my.jcu.edu.au>
2024-11-04 19:43:49 +0800

    Added function to list all ongoing and completed project

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -77,3 +77,126 @@ def main():
     autosave()
     print("Thank you for using custom-built project management software.")
 
+def restore_datetime():
+    """This function will date object into string object"""
+    for project in class_projects:
+        project.start_date = project.start_date.strftime("%d/%m/%Y")
+
+def update_project(new_percentage, new_priority, project_choice):
+    """This function will update the projects"""
+    class_projects[project_choice] = Project(class_projects[project_choice].name,
+                                             class_projects[project_choice].start_date, int(new_priority),
+                                             float(class_projects[project_choice].cost_estimate),
+                                             int(new_percentage))
+
+def sort_project_by_date(date, sorted_filtered_projects):
+    """This function sort project by date"""
+    filtered_projects = []
+    filtered_dates = []
+    final_sorted_filtered_dates = []
+    filter_projects(date, filtered_projects)
+    filter_dates(filtered_dates, filtered_projects)
+    sorted_filtered_dates = [date for date in sorted(filtered_dates)]
+    remove_duplicated_dates(final_sorted_filtered_dates, sorted_filtered_dates)
+    for date in final_sorted_filtered_dates:
+        for project in filtered_projects:
+            if date == project.start_date:
+                sorted_filtered_projects.append(project)
+
+def remove_duplicated_dates(final_sorted_filtered_dates, sorted_filtered_dates):
+    """This function will check if there is a duplicate dates """
+    for date in sorted_filtered_dates:
+        if date not in final_sorted_filtered_dates:
+            final_sorted_filtered_dates.append(date)
+
+def filter_dates(filtered_dates, filtered_projects):
+    """This function will add the filtered project to the list"""
+    for project in filtered_projects:
+        filtered_dates.append(project.start_date)
+
+def filter_projects(date, filtered_projects):
+    """This function convert data to date and add to the list if the data is greater than date"""
+    for project in class_projects:
+        project.start_date = datetime.datetime.strptime(str(project.start_date), "%d/%m/%Y").date()
+        if project.start_date > date:
+            filtered_projects.append(project)
+
+def validate_project_choice():
+    """This function will add a project and check if there is a existed project in the file"""
+    try:
+        project_choice = int(input("Project choice: "))
+    except ValueError:
+        print("Invalid Choice")
+        project_choice = int(input("Project choice: "))
+    while project_choice not in project_dictionary:
+        print("Invalid Choice")
+        project_choice = int(input("Project choice: "))
+    return project_choice
+
+def validate_date():
+    """This function will ask the user for the date and check if its valid or not"""
+    data = input("Start date (dd/mm/yy): ")
+    while len(data) < 8 or len(data) > 10 or data.count("/") != 2:
+        print("Invalid input")
+        data = input("Start date (dd/mm/yy): ")
+    return data
+
+def validate_cost_estimate():
+    """This function will ask the user for the cost estimate"""
+    try:
+        data = float(input("Cost estimate: $"))
+    except ValueError:
+        print("Invalid input")
+        data = float(input("Cost estimate: $"))
+    return data
+
+def validate_priority(prompt):
+    """This function will ask the user for the priority for the project"""
+    try:
+        data = int(input(prompt))
+    except ValueError:
+        print("Invalid input")
+        data = int(input(prompt))
+    while data < 0 or data > 10:
+        print("Invalid input")
+        data = int(input(prompt))
+    return data
+
+def validate_percentage(prompt):
+    """This function will ask user for the percentage of the project"""
+    try:
+        data = int(input(prompt))
+    except ValueError:
+        print("Invalid input")
+        data = int(input(prompt))
+    while data < 0 or data > 100:
+        print("Invalid input")
+        data = int(input(prompt))
+    return data
+
+def autosave():
+    """This function will save the file"""
+    output_file = open("projects.txt", "w")
+    print("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage", file=output_file)
+    for project in class_projects:
+        print(
+            f"{project.name}\t{project.start_date}\t{project.priority}\t{project.cost_estimate}\t{project.completion_percentage}",
+            file=output_file)
+    output_file.close()
+
+def get_choice():
+    """This function will ask the user for a choice"""
+    print(MENU, end="\n")
+    choice = input(">>> ").upper()
+    return choice
+
+def extract_data(data):
+    """This function will extract data from the project.txt"""
+    projects = []
+    for line in data[1:]:
+        items = line.strip().split("\t")
+        projects.append(items)
+    for project in projects:
+        class_projects.append(Project(project[0], project[1], int(project[2]), float(project[3]), int(project[4])))
+
+main()
